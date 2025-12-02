294853046476f4ded0d3f9e19f6ad0c725e936cd
Takalani <takalani@bazaruto.com>
2025-12-03 01:53:14 +0200

    Fix tests and finalise to do UI

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,69 +1,53 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: %i[ show edit update destroy ]
 
-  # GET /todos or /todos.json
   def index
     @todos = Todo.all
   end
 
-  # GET /todos/1 or /todos/1.json
   def show
   end
 
-  # GET /todos/new
   def new
     @todo = Todo.new
   end
 
-  # GET /todos/1/edit
   def edit
   end
 
-  # POST /todos or /todos.json
   def create
     @todo = Todo.new(todo_params)
 
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to @todo, notice: "Todo was successfully created." }
-        format.json { render :show, status: :created, location: @todo }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
+    if @todo.save
+      redirect_to @todo, notice: "Todo was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /todos/1 or /todos/1.json
   def update
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: "Todo was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @todo }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
+    if @todo.update(todo_params)
+      redirect_to @todo, notice: "Todo was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /todos/1 or /todos/1.json
   def destroy
     @todo.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to todos_path, notice: "Todo was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
+    if @todo.destroyed?
+      redirect_to todos_path, notice: "Todo was successfully destroyed.", status: :see_other
+    else
+      redirect_to todos_path, alert: "Todo could not be destroyed.", status: :unprocessable_entity
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_todo
       @todo = Todo.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def todo_params
       params.require(:todo).permit(:title, :description, :status)
     end
